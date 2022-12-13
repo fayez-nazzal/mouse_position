c333da2c1f9a0f5f694639bdf24b1baf39632518
Fayez Nazzal <soulldev@gmail.com>
2022-12-13 09:44:36 +0200

    Use xlib for linux instead of Command

--- src/impl_linux.rs
+++ src/impl_linux.rs
@@ -1,41 +1,54 @@
-use std::io;
+
 use crate::mouse_position::Mouse;
 
 #[cfg(target_os = "linux")]
 impl Mouse {
     pub fn get_mouse_position() -> Mouse {
-        use std::{process::{Command, Stdio}, io::{Read, BufReader}};
+        use x11_dl::xlib;
 
-        let output = match Command::new("xdotool")
-                .arg("getmouselocation")
-                .arg("--shell")
-                .stdout(Stdio::piped())
-                .spawn() {
-            Ok(it) => it,
-            Err(_) => return Mouse::Error
+        // Initialize Xlib and open a connection to the X server
+        let xlib = xlib::Xlib::open().unwrap();
+        let display = unsafe { (xlib.XOpenDisplay)(std::ptr::null()) };
+
+        // Get the root window for the current screen
+        let screen = unsafe { (xlib.XDefaultScreen)(display) };
+        let root = unsafe { (xlib.XRootWindow)(display, screen) };
+
+        // Get the pointer position
+        let mut root_return: xlib::Window = 0;
+        let mut child_return: xlib::Window = 0;
+        let mut root_x_return: i32 = -1;
+        let mut root_y_return: i32 = -1;
+        let mut win_x_return: i32 = 0;
+        let mut win_y_return: i32 = 0;
+        let mut mask_return: u32 = 0;
+
+        unsafe {
+            (xlib.XQueryPointer)(
+                display,
+                root,
+                &mut root_return,
+                &mut child_return,
+                &mut root_x_return,
+                &mut root_y_return,
+                &mut win_x_return,
+                &mut win_y_return,
+                &mut mask_return,
+            );
         }
-            .stdout
-            .ok_or(io::Error::new(io::ErrorKind::Other, "Failed to capture xdotool output")).unwrap();
-    
-        let mut reader = BufReader::new(output);
-        let mut buffer = String::new();
-        reader.read_to_string(&mut buffer).unwrap();
-    
-        let mut x = 0;
-        let mut y = 0;
-    
-        for line in buffer.lines() {
-            let mut parts = line.split('=');
-            let key = parts.next().unwrap();
-            let value = parts.next().unwrap();
-    
-            if key == "X" {
-                x = value.parse().unwrap();
-            } else if key == "Y" {
-                y = value.parse().unwrap();
-            }
+
+        // Close the connection to the X server
+        unsafe {
+            (xlib.XCloseDisplay)(display);
         }
-    
-        Mouse::Position { x, y }
+
+        if root_x_return == -1 || root_y_return == -1 {
+            return Mouse::Error;
+        }
+
+        Mouse::Position {
+            x: root_x_return,
+            y: root_y_return,
+        }
     }
 }
--- src/impl_windows.rs
+++ src/impl_windows.rs
@@ -1,9 +1,11 @@
-use winapi::{um::winuser::{GetCursorPos}, shared::windef::POINT};
+
 use crate::mouse_position::Mouse;
 
 #[cfg(target_os = "windows")]
 impl Mouse {
     pub fn get_mouse_position() -> Mouse {
+        use winapi::{um::winuser::{GetCursorPos}, shared::windef::POINT};
+
         let mut point = POINT { x: 0, y: 0 };
         let result = unsafe { GetCursorPos(&mut point) };
 
