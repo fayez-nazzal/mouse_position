f2b5e0d2ee77cd9c500fb1df98879da257a1d23a
Apocalyptapig <101874145+Apocalyptapig@users.noreply.github.com>
2023-01-13 23:19:30 -0600

    windows-only function for setting DPI

--- src/impl_windows.rs
+++ src/impl_windows.rs
@@ -5,7 +5,10 @@ use crate::mouse_position::Mouse;
 #[cfg(target_os = "windows")]
 impl Mouse {
     pub fn get_mouse_position() -> Mouse {
-        use winapi::{um::winuser::{GetCursorPos}, shared::windef::POINT};
+        use winapi::{
+            um::winuser::GetCursorPos,
+            shared::windef::POINT,
+        };
 
         let mut point = POINT { x: 0, y: 0 };
         let result = unsafe { GetCursorPos(&mut point) };
@@ -15,5 +18,19 @@ impl Mouse {
         }
 
         Mouse::Error
+    }
+
+    pub fn set_dpi(monitor: usize) {
+        use winapi::um::shellscalingapi::SetProcessDpiAwareness;
+        use winit::event_loop::EventLoop;
+
+        // forgive me
+        let mut monitors = (*EventLoop::new()).available_monitors();
+        let dpi = match monitors.nth(monitor) {
+            Some(monitor) => monitor.scale_factor() * 100.0,
+            None => panic!("Invalid monitor ID (counts from 0)")
+        };
+
+        unsafe { SetProcessDpiAwareness(dpi as u32); }
     }
 }
